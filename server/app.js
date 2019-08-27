d60707d8df9f70cb76b3ec5590bbfea64e1af559
Awais Manzoor <awais.manzoor@phenologix.com>
2019-08-27 21:54:38 +0500

    coinbase in project and removed some error save till 9:54 8/27/2019

--- server/app.js
+++ server/app.js
@@ -32,6 +32,7 @@ app.use('/api-docs',require("./routes/documentation.route"));
 app.use('/api/products', require('./routes/product.routes'));
 app.use('/api/user', require('./routes/users.routes'));
 app.use('/api/card', require('./routes/card.routes'));
+app.use('/api/checkout', require('./routes/checkout.routes'));
 // app.use('/api/clients', require('./routes/clients.routes'));
 // app.use('/api/suppliers', require('./routes/suppliers.routes'));
 /**
---
+++ server/controllers/checkout.controller.js
@@ -0,0 +1,41 @@
+import Card from "../models/card";
+import Product from "../models/products";
+import { Coinbase_API_Key } from "../config/config";
+var coinbase = require("coinbase-commerce-node");
+var Client = coinbase.Client;
+var Charge = coinbase.resources.Charge;
+Client.init(Coinbase_API_Key);
+
+const checkout = {};
+
+checkout.createCharge = async (req, res) => {
+  var firstChargeObj = new Charge({
+    description: "Mastering the Transition to the Information Age",
+    metadata: {
+      customer_id: "id_1005",
+      customer_name: "Satoshi Nakamoto"
+    },
+    name: "Test Name",
+    payments: [],
+    pricing_type: "no_price"
+  });
+  firstChargeObj.save(function(error, response) {
+    console.log("Created charge(callback)");
+    console.log(response);
+    console.log(error);
+
+    if (response && response.id) {
+      Charge.retrieve(response.id, function(error, response) {
+        console.log("Retrived charge(callback)");
+        console.log(response);
+        console.log(error);
+      });
+    }
+  });
+};
+
+export default checkout;
+
+/** this ends this file
+ * server/controllers/card.controller
+ **/
