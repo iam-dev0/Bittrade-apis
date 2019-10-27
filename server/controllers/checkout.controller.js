fc32dfbd75056e8c72c7e96a3f4420d0d984e239
awais000 <awaismanzoor00@gmail.com>
2019-10-27 17:12:39 +0500

    change checkout Function

--- server/controllers/checkout.controller.js
+++ server/controllers/checkout.controller.js
@@ -10,109 +10,77 @@ const checkout = {};
 
 checkout.createCharge = async (req, res) => {
   const { id } = req.params;
-  let userData, products=[],LumSum;
-  await Card.find({ userId: id }, async (err, data) => {
+  const { price } = req.query;
+  let userData;
+  User.findById(id, (err, response) => {
     if (err) {
-      res.status(422).json({
+      res.status(400).json({
         success: false,
         message:
           "Sorry Something Happened We'll get back to you as soon as possible",
         error: err
       });
-      return;
+      return null;
     }
-    if (data) {
-      products = data;
-      // Card.remove({ userId: id }, async (err, data) => {
-      //   if (err) {
-      //     res.status(400).json({
-      //       success: false,
-      //       message:
-      //         "Sorry Something Happened We'll get back to you as soon as possible",
-      //       error: err
-      //     });
-      //   } else {
-      //     if (!data.length) {
-      //       res.status(404).json({
-      //         success: true,
-      //         message: "Product doesn't exist"
-      //       });
-      //     }
-      //     // res.status(200).json({
-      //     //   success: true,
-      //     //   message: "Products removed Successfully",
-      //     //   data
-      //     // });
-      //   }
-     // });
-    } else {
-      res.status(400).json({ success: false, message: "" });
-    }
-  });
-
-  if (products.length > 0) {
-    await User.findById(id)
-      .then(response => {
-        userData = response;
-      })
-      .catch(err => {
-        res.status(400).json({
-          success: false,
-          message:
-            "Sorry Something Happened We'll get back to you as soon as possible",
-          error: err
-        });
-        return;
-      });
- LumSum=LumSumPrice(products);
-    var firstChargeObj = new Charge({
-      description: `${userData.email}`,
-      metadata: {
-        "customer_id": "id_1005",
-        "customer_name": "Name"
-      },
-      name: "BitTrade",
-      payments: [],
-      local_price: {
-        amount:  `${LumSum}`,
-        currency: "USD"
-      },
-      pricing_type: "fixed_price"
-      // pricing: {
-      //   local: { amount: '100.00', currency:'USD' },
-      //   // "bitcoin": { "amount": "1.00", "currency": "BTC" },
-      //   // "ethereum": { "amount": "10.00", "currency": "ETH" }
-      // },
-    });
-
-    firstChargeObj.save(function(error, response) {
-      console.log("Created charge(callback)");
-      console.log(response);
-      console.log(error);
-
-      if (response && response.id) {
-        Charge.retrieve(response.id, function(error, response) {
-          console.log("Retrived charge(callback)");
-          console.log(response);
-          console.log(error);
+    if (response) {
+      userData = response;
+      let ChargeObj = createCharge({ userData, price });
+      ChargeObj.save((error, response) => {
+        if (error) {
+          res.status(422).json({
+            success: false,
+            message:
+              "Sorry Something Happened With the Coinbase Server We'll get back to you as soon as possible",
+            error: error
+          });
+          return null;
+        }
+        if (response && response.id) {
           res.status(200).json({
             success: true,
             message: "",
             hosted_url: response.hosted_url
           });
-          return;
+          return null;
+        }
+        res.status(422).json({
+          success: false,
+          message: "Sorry Can't Host the URL Because of An Issue"
         });
-      }
-    });
-  }
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Can't Find the User"
+      });
+      return null;
+    }
+  });
 };
 
-const LumSumPrice= (products) => {
- let price=0;
- products.map((item)=>{
-  price=price+item.product.price*item.product.quantity;
- })
- return price;
+const createCharge = ({ userData, price }) => {
+  return new Charge({
+    description: `${userData.email}`,
+    metadata: {
+      customer_id: "id_1005",
+      customer_name: "Name"
+    },
+    name: "BitTrade",
+    payments: [],
+    local_price: {
+      amount: `${price}`,
+      currency: "USD"
+    },
+    pricing_type: "fixed_price"
+  });
+};
+
+const LumSumPrice = products => {
+  let price = 0;
+  products.map(item => {
+    price = price + item.product.price * item.product.quantity;
+  });
+  return price;
 };
 export default checkout;
 
